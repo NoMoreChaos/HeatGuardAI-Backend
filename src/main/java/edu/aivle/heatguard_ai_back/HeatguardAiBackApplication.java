f5327231f59889755900c8e8e67a734d7bc9680f
GrabSomeChicken <45869387+CiSJiK@users.noreply.github.com>
2026-01-13 15:40:01 +0900

    Init files

---
+++ src/main/java/edu/aivle/heatguard_ai_back/HeatguardAiBackApplication.java
@@ -0,0 +1,13 @@
+package edu.aivle.heatguard_ai_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeatguardAiBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeatguardAiBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/aivle/heatguard_ai_back/HeatguardAiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.aivle.heatguard_ai_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeatguardAiBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
